efa97eba80e5fd965186655fe0016b47c5e21c97
Terumi Kusaka <t.kusaka3@gmail.com>
2018-06-24 17:28:21 -0400

    deploy

--- Front-end/server.js
+++ Front-end/server.js
@@ -1,11 +1,16 @@
 const express = require('express'),
     path = require('path');
+
 const app = express();
-app.use(express.static('./dist/front-end'));
+
+app.use(express.static('./dist/Front-end'));
+
+
 app.get('/*', (req, res)=>{
-  res.sendFile(path.join(__dirname + '/dist/front-end/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/Front-end/index.html'));
 });
+
 app.listen(process.env.PORT || 8080, ()=>{
     console.log('server started');
+    
 })
-
