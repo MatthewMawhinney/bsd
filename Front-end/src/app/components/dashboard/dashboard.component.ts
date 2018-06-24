09f65138adcca307a360e26188273684c9120bb7
Geri Kona <gerikona@hotmail.com>
2018-06-23 21:51:06 -0400

    Place component onClick selects map marker

--- Front-end/src/app/components/dashboard/dashboard.component.ts
+++ Front-end/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { RegisterService } from '../../services/register.service';
-import { FlashMessagesService } from 'angular2-flash-messages';
-import { Router } from '@angular/router';
-import { CommsService } from '../../services/comms.service';
-import { LocationService } from '../../services/location.service';
-import { Geolocation } from '../search/geolocation';
+import {Component, OnInit} from '@angular/core';
+import {RegisterService} from '../../services/register.service';
+import {FlashMessagesService} from 'angular2-flash-messages';
+import {Router} from '@angular/router';
+import {CommsService} from '../../services/comms.service';
+import {LocationService} from '../../services/location.service';
+import {Geolocation} from '../search/geolocation';
 
 @Component({
   selector: 'app-dashboard',
@@ -23,7 +23,8 @@ export class DashboardComponent implements OnInit {
     private flashMessage: FlashMessagesService,
     private comms: CommsService,
     private locationService: LocationService
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     this.comms.newSearch.subscribe(data => this.newSearch = data);
@@ -31,7 +32,7 @@ export class DashboardComponent implements OnInit {
 
   swapTags() {
     let tags = document.getElementById("tags");
-    if(tags.classList.contains('sr-only')) {
+    if (tags.classList.contains('sr-only')) {
       tags.classList.remove('sr-only');
     } else {
       tags.classList.add('sr-only');
@@ -43,10 +44,11 @@ export class DashboardComponent implements OnInit {
       this.gpsLocation = this.locationService.requestGeoLoc();
       this.comms.changeLocation(this.gpsLocation);
     } else {*/
-      this.geocoding(this.userLoc);
+    //this.geocoding(this.userLoc);
     //}
 
-    this.updateSearch(this.searchFilter);
+    this.comms.changeSearch(this.searchFilter);
+    //this.updateSearch(this.searchFilter);
 
     // this.comms.toggleSearch(true);
     setTimeout(() => {
@@ -58,28 +60,28 @@ export class DashboardComponent implements OnInit {
   }
 
   // Clear search filter
-  clearSearch() {
-    this.updateSearch('');
-  }
+  // clearSearch() {
+  //   this.updateSearch('');
+  // }
 
   // Function to update 'search' in CommsService
-  updateSearch(search: string) {
-    this.comms.changeSearch(search);
-  }
+  // updateSearch(search: string) {
+  //   this.comms.changeSearch(search);
+  // }
 
-  public geocoding(userLoc: string) {
-    // pass the current location to MapService.geocoding()
-    this.locationService.geocoding(userLoc).then(
-      rtn => {
-        const location = rtn[0].geometry.location;
-        // get lat and long and then assign to the props
-        const latlng: Geolocation = new Geolocation();
-        latlng.lat = location.lat();
-        latlng.lng = location.lng();
-        this.comms.changeLocation(latlng);
-      }
-    );
-  }
+  // public geocoding(userLoc: string) {
+  //   // pass the current location to MapService.geocoding()
+  //   this.locationService.geocoding(userLoc).then(
+  //     rtn => {
+  //       const location = rtn[0].geometry.location;
+  //       // get lat and long and then assign to the props
+  //       const latlng: Geolocation = new Geolocation();
+  //       latlng.lat = location.lat();
+  //       latlng.lng = location.lng();
+  //       this.comms.changeLocation(latlng);
+  //     }
+  //   );
+  // }
 
   onLogoutClick() {
     this.registerService.logout();
